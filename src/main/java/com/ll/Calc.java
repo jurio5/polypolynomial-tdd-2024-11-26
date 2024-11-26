8f9bcfe3ddde140fff31434e3b9e0c1841a25fd3
jurio5 <sia5492@naver.com>
2024-11-26 12:05:41 +0900

    work

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+public class Calc {
+    public static int run(String expr) {
+        String[] exprBits = expr.split(" \\+ ");
+        int num1 = Integer.parseInt(exprBits[0]);
+        int num2 = Integer.parseInt(exprBits[1]);
+
+        return num1 + num2;
+    }
+}
